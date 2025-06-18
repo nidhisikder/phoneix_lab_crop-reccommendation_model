c348ea8b0d2cd58ee574387e9ef39877fd6d8f80
Nidhi Sikder <nidhisbkp@gmail.com>
2025-06-18 17:59:19 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import gradio as gr
+import joblib
+import numpy as np
+model=joblib.load("model.pkl")
+# Values taken through real-time input: 
+def predict(N, P, K, Temp, Humidity, pH, Rainfall):
+    input_data=np.array([[N, P, K, Temp, Humidity, pH, Rainfall]])
+    predicted_crop = model.predict(input_data)
+    return f"Recommended Crop:{ predicted_crop[0]}"
+demo=gr.Interface(fn=predict,inputs=[gr.Number(label="N"),
+                                     gr.Number(label="P"),
+                                     gr.Number(label="K"),
+                                     gr.Number(label="Temp"),
+                                     gr.Number(label="Humidity"),
+                                     gr.Number(label="pH"),
+                                     gr.Number(label="Rainfall")],output="This is the recommended crop")
+if(__name__=="__main__"):
+    demo.launch()
